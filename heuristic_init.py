5c677c21bef134bc130b3d03bbc23a89761000e1
Duc <hdrake1302@gmail.com>
2021-02-03 12:11:52 +0700

    Update A* search and correct some datas

--- heuristic_init.py
+++ heuristic_init.py
@@ -3,8 +3,8 @@ import json
 heuristic = {'Arad': 366, 'Bucharest': 0, 'Craiova': 160, 'Drobeta': 242,
                      'Eforie': 161, 'Giurgiu': 77, 'Hirsova': 151, 'Iasi': 226,
                      'Lugoj': 244, 'Mehadia': 241, 'Neamt': 234, 'Oradea': 380,
-                     'Pitesti': 100, 'Rimnicu Vikea': 193, 'Siubiu': 253,
-                     'Timisoara': 329, 'Urzkeni': 80, 'Vaslui': 199, 'Zerind': 374}
+                     'Pitesti': 100, 'Rimnicu Vilcea': 193, 'Siubiu': 253, 'Fagaras': 176,
+                     'Timisoara': 329, 'Urziceni': 80, 'Vaslui': 199, 'Zerind': 374}
 
-        with open("heuristic_data.json", "w") as write_file:
-            json.dump(heuristic, write_file, indent=4)
+with open("heuristic_data.json", "w") as write_file:
+    json.dump(heuristic, write_file, indent=4)
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import json
 
 import que as q
 
+from queue import PriorityQueue
+
+
 class Graph:
     def __init__(self):
         self.graph = dict()
@@ -33,8 +36,8 @@ class Graph:
 
         while not queue.empty():
             x = queue.dequeue()
-
             print(x)
+
             for city in self.graph[x]:
                 if visited[city[0]] is False:
                     visited[city[0]] = True
@@ -47,11 +50,58 @@ class Graph:
 
         return heuristic[n]
 
+    def heuristic_search(self, start, end):
+        """Perform inform search using heuristic algorithm"""
+        queue = PriorityQueue()
+        queue.put((self.h(start), start))
 
+        # visited variable to check if a city is visited or not
+        visited = dict()
+        for city in self.graph:
+            visited[city] = False
 
+        while not queue.empty():
+            x = queue.get()
+            current_city = x[1]
+            visited[current_city] = True
+
+            print(x)
+            if current_city == end:
+                break
+
+            for city in self.graph[current_city]:
+                if visited[city[0]] is False:
+                    queue.put((self.h(city[0]), city[0]))
+
+    def A_search(self, start, end):
+        """Perform inform search using A* algorithm"""
+        queue = PriorityQueue()
+        queue.put((self.h(start) + 0, 0, start))
+
+        # visited variable to check if a city is visited or not
+        visited = dict()
+        for city in self.graph:
+            visited[city] = False
+
+        while not queue.empty():
+            x = queue.get()
+            current_city = x[2]
+            visited[current_city] = True
+
+            cost = x[1]
+            print(x)
+            if current_city == end:
+                break
+
+            for city in self.graph[current_city]:
+                tmp_cost = city[1] + cost
+                if visited[city[0]] is False:
+                    queue.put((self.h(city[0]) + tmp_cost, tmp_cost, city[0]))
 
 
 g = Graph()
 
 with open("graph_data.json", "r") as read_file:
     g.graph = json.load(read_file)
+
+g.A_search('Arad', 'Bucharest')
